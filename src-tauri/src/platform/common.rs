e36ce56d6755670320ad74631a27cf7bacddc31a
ilge.ustun <153606956+ilge-ustun@users.noreply.github.com>
2025-08-13 11:23:39 +0200

    no system tray

--- src-tauri/src/platform/common.rs
+++
@@ -1,9 +0,0 @@
-use super::PlatformInterface;
-
-pub struct UnsupportedPlatform;
-
-impl PlatformInterface for UnsupportedPlatform {
-    fn setup_system_tray() -> Result<(), String> {
-        Err("System tray not supported on this platform".to_string())
-    }
-}
--- src-tauri/src/platform/macos.rs
+++
@@ -1,9 +0,0 @@
-use super::PlatformInterface;
-
-pub struct MacOSPlatform;
-
-impl PlatformInterface for MacOSPlatform {
-    fn setup_system_tray() -> Result<(), String> {
-        Ok(())
-    }
-}
