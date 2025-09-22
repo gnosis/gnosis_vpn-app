03cf32fc9bb2d1485b864bfba202a9d54759bda1
ilge.ustun <153606956+ilge-ustun@users.noreply.github.com>
2025-09-22 15:00:16 +0200

    airdrop css

--- src/components/AirdropClaim.tsx
+++ src/components/AirdropClaim.tsx
@@ -2,9 +2,9 @@ import Button from './common/Button';
 
 export default function AirdropClaim() {
   return (
-    <div class="rounded-xl bg-[#E2F5FF] px-4 py-2">
+    <div class="rounded-xl bg-[#E2F5FF] px-4 py-2 w-full">
       <div class="font-bold">Only relevant for Testers:</div>
-      <div class="text-sm">If you’re a tester, claim here wxHOPR and xDAI.</div>
+      <div class="text-sm">Claim here wxHOPR and xDAI.</div>
       <div class="flex justify-center mt-1">
         <Button>Claim Airdrop</Button>
       </div>
