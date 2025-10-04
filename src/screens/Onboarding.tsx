f26aaa5170db5b04ddbbf6c8c06ca090327a45c9
ilge.ustun <153606956+ilge-ustun@users.noreply.github.com>
2025-10-05 00:14:13 +0200

    cleanup

--- src/screens/Onboarding.tsx
+++ src/screens/Onboarding.tsx
@@ -3,7 +3,6 @@ import Start from "@src/components/onboarding/Start";
 import Airdrop from "@src/components/onboarding/Airdrop";
 import { Dynamic } from "solid-js/web";
 import Complete from "@src/components/onboarding/Complete";
-import { useAppStore } from "@src/stores/appStore";
 
 const steps = {
   start: Start,
@@ -15,9 +14,6 @@ export default function Onboarding() {
   const [step, setStep] = createSignal<"start" | "airdrop" | "complete">(
     "start",
   );
-
-  const [appState, appActions] = useAppStore();
-  console.log("appState", appState);
 
   return (
     <div class="h-screen bg-gray-100">
