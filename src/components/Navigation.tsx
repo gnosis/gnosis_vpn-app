931cc41265113b2c19d164608ccd06a1cad7982a
ilge.ustun <153606956+ilge-ustun@users.noreply.github.com>
2025-11-18 09:10:20 -0300

    funding status icon

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -2,13 +2,16 @@ import IconButton from "@src/components/common/IconButton.tsx";
 import { Portal } from "solid-js/web";
 import settingsIcon from "@assets/icons/settings.svg";
 import logsIcon from "@assets/icons/logs.svg";
-// import fundsFullIcon from "@assets/icons/funds-full.svg";
-// import fundsLowIcon from "@assets/icons/funds-low.svg";
-// import fundsOutIcon from "@assets/icons/funds-out.svg";
+import fundsFullIcon from "@assets/icons/funds-full.svg";
+import fundsLowIcon from "@assets/icons/funds-low.svg";
+import fundsOutIcon from "@assets/icons/funds-out.svg";
 import fundsEmptyIcon from "@assets/icons/funds-empty.svg";
 import { WebviewWindow } from "@tauri-apps/api/webviewWindow";
+import { useAppStore } from "@src/stores/appStore";
+import { isRunningRunMode } from "@src/services/vpnService";
 
 function Navigation() {
+  const [appState] = useAppStore();
   const openSettingsWindow = async (target?: "settings" | "usage" | "logs") => {
     const settingsWin = await WebviewWindow.getByLabel("settings");
     if (settingsWin) {
@@ -20,28 +23,30 @@ function Navigation() {
     }
   };
 
-  // const getFundsIcon = () => {
-  //   const status = appState.fundingStatus;
-  //   if (status === "WellFunded") {
-  //     return fundsFullIcon;
-  //   }
-  //   if (typeof status === "object" && "TopIssue" in status) {
-  //     switch (status.TopIssue) {
-  //       case "Unfunded":
-  //       case "ChannelsOutOfFunds":
-  //         return fundsEmptyIcon;
-  //       case "SafeLowOnFunds":
-  //       case "NodeLowOnFunds":
-  //         return fundsLowIcon;
-  //       case "NodeUnderfunded":
-  //       case "SafeOutOfFunds":
-  //         return fundsOutIcon;
-  //     }
-  //   }
-  //   return fundsEmptyIcon;
-  // };
+  const getFundsIcon = () => {
+    const status = isRunningRunMode(appState.runMode)
+      ? appState.runMode.Running.funding
+      : undefined;
+    if (status === "WellFunded") {
+      return fundsFullIcon;
+    }
+    if (typeof status === "object" && "TopIssue" in status) {
+      switch (status.TopIssue) {
+        case "Unfunded":
+        case "ChannelsOutOfFunds":
+          return fundsEmptyIcon;
+        case "SafeLowOnFunds":
+        case "NodeLowOnFunds":
+          return fundsLowIcon;
+        case "NodeUnderfunded":
+        case "SafeOutOfFunds":
+          return fundsOutIcon;
+      }
+    }
+    return fundsEmptyIcon;
+  };
 
-  const getFundsIcon = () => fundsEmptyIcon;
+  // const getFundsIcon = () => fundsEmptyIcon;
 
   return (
     <Portal>
