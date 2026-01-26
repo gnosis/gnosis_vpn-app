61f8c490c57896339e7a145bb69be7ad6efe5761
ilge.ustun <153606956+ilge-ustun@users.noreply.github.com>
2026-01-26 12:16:34 +0100

    debugging tray icons

--- src-tauri/src/icons.rs
+++ src-tauri/src/icons.rs
@@ -86,7 +86,11 @@ pub fn determine_app_icon(connection_state: &str, run_mode: &command::RunMode) -
 }
 
 pub fn determine_tray_icon(connection_state: &str, theme: Option<Theme>) -> &'static str {
-    let use_black_icons = !cfg!(target_os = "macos") && matches!(theme, Some(Theme::Light));
+    let use_black_icons = !cfg!(target_os = "macos") && matches!(theme, Some(Theme::Dark));
+    println!("use_black_icons: {}", use_black_icons);
+    println!("connection_state: {}", connection_state);
+    println!("theme: {:?}", theme);
+
     match (connection_state, use_black_icons) {
         ("Connected", true) => TRAY_ICON_CONNECTED_BLACK,
         ("Connected", false) => TRAY_ICON_CONNECTED,
