f501995f3c83913b0920222c855170dedf0369f5
Ronny Esterluss <ronny.esterluss@hoprnet.org>
2026-02-01 09:32:02 +0100

    use theme API for change handling

--- src/index.tsx
+++ src/index.tsx
@@ -7,15 +7,7 @@ import SettingsWindow from "./windows/SettingsWindow.tsx";
 import { getCurrentWindow } from "@tauri-apps/api/window";
 import { useAppStore } from "./stores/appStore.ts";
 import { invoke } from "@tauri-apps/api/core";
-import { createResource } from "solid-js";
-
-function applyTheme(isDark: boolean) {
-  if (isDark) {
-    document.documentElement.classList.add("dark");
-  } else {
-    document.documentElement.classList.remove("dark");
-  }
-}
+import { createResource, onCleanup, onMount } from "solid-js";
 
 function screenFromLabel(label: string) {
   if (label === "settings") {
@@ -24,22 +16,7 @@ function screenFromLabel(label: string) {
   return <App />;
 }
 
-function handleThemeChange(event: MediaQueryListEvent) {
-  const theme = event.matches ? "dark" : "light";
-  invoke("theme_changed", { theme });
-}
-
 (() => {
-  // initial theme and theme change listener
-  const darkMedia = globalThis.matchMedia("(prefers-color-scheme: dark)");
-  if (typeof darkMedia.addEventListener === "function") {
-    darkMedia.addEventListener("change", handleThemeChange);
-  } else {
-    darkMedia.onchange = handleThemeChange;
-  }
-  applyTheme(darkMedia.matches);
-
-  // initialize rendering
   const root = document.getElementById("root") as HTMLElement;
   const [_settings, settingsActions] = useSettingsStore();
   const [,] = useAppStore();
@@ -48,15 +25,33 @@ function handleThemeChange(event: MediaQueryListEvent) {
 
   // cannot use Suspense here because screen has a hard requirements on settings being loaded
   render(
-    () => (
-      <Show
-        when={loadSettings.state === "ready"}
-        // TODO needs better fallback or splash screen
-        fallback={<div>Loading...</div>}
-      >
-        {screenFromLabel(curWindow.label)}
-      </Show>
-    ),
+    () => {
+      onMount(async () => {
+        const theme = await curWindow.theme();
+        invoke("theme_changed", { theme });
+
+        const unlistenCb = await curWindow.onThemeChanged(
+          ({ payload: theme }) => {
+            console.log("theme", theme);
+            invoke("theme_changed", { theme });
+          },
+        );
+
+        onCleanup(() => {
+          unlistenCb();
+        });
+      });
+
+      return (
+        <Show
+          when={loadSettings.state === "ready"}
+          // TODO needs better fallback or splash screen
+          fallback={<div>Loading...</div>}
+        >
+          {screenFromLabel(curWindow.label)}
+        </Show>
+      );
+    },
     root,
   );
 })();
