7c5d26964bbcf920755b98de2e560eb4a0204c6c
Ronny Esterluss <ronny.esterluss@hoprnet.org>
2026-03-13 21:49:53 +0100

    chore(version): bump to v0.20.0

--- src/utils/compatibility.ts
+++ src/utils/compatibility.ts
@@ -1,4 +1,4 @@
-export const COMPATIBLE_VERSIONS: string[] = ["0.80", "0.81"];
+export const COMPATIBLE_VERSIONS: string[] = ["0.81"];
 
 /**
  * Checks if the provided service version string is compatible
