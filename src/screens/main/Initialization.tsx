84522c9ffaeb98cb997c9b2d92498063bed3e02f
Ronny Esterluss <ronny.esterluss@hoprnet.org>
2026-03-25 17:40:56 +0100

    showing app version in init

--- src/screens/main/Initialization.tsx
+++ src/screens/main/Initialization.tsx
@@ -24,10 +24,14 @@ export default function Initialization(props: InitializationProps) {
             <Spinner class="mb-4 h-8 w-8 text-brand-primary" />
             <p class="text-text-secondary">Running startup tasks</p>
             {props.info && (
-              <p class="mt-2 text-xs text-text-tertiary">
-                Service Version: {props.info.version}
-                App Version: {props.appVersion}
-              </p>
+              <>
+                <p class="mt-2 text-xs text-text-tertiary">
+                  Service Version: {props.info.version}
+                </p>
+                <p class="mt-2 text-xs text-text-tertiary">
+                  App Version: {props.appVersion}
+                </p>
+              </>
             )}
           </div>
         )}
