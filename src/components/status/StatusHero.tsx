d0623b3b37258497d5bbfff6c3a9bbadd542464f
Ronny Esterluss <ronny.esterluss@hoprnet.org>
2026-04-01 12:17:51 +0200

    feat: immediately switch to disconnected when disconnecting

--- src/components/status/StatusHero.tsx
+++ src/components/status/StatusHero.tsx
@@ -7,20 +7,23 @@ import disconnectedImg from "@assets/img/disconnected.svg";
 export function StatusHero() {
   const [appState] = useAppStore();
 
-  const isBusy = () =>
-    appState.vpnStatus === "Connecting" ||
-    appState.vpnStatus === "Disconnecting";
+  const isConnecting = () => appState.vpnStatus === "Connecting";
+
+  const showDisconnected =
+    appState.vpnStatus === "Disconnected" ||
+    appState.vpnStatus === "Disconnecting" ||
+    !appState.runMode;
 
   return (
     <div class="w-full h-1/3 flex flex-col items-center justify-center gap-3">
       <div class="h-24 flex items-center justify-center">
-        <Show when={isBusy()}>
+        <Show when={isConnecting()}>
           <SpinnerBig />
         </Show>
         <Show when={appState.vpnStatus === "Connected"}>
           <img src={connectedImg} alt="Connected" class="h-24" />
         </Show>
-        <Show when={appState.vpnStatus === "Disconnected" || !appState.runMode}>
+        <Show when={showDisconnected}>
           <img src={disconnectedImg} alt="Disconnected" class="h-24" />
         </Show>
       </div>
