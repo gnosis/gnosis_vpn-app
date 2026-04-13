c87fcc0e4f6cb83b526d9fa17e7fb80450d66ac8
VM <michal.jadach@hoprnet.org>
2026-04-13 11:30:25 +0200

    format

--- flake.nix
+++ flake.nix
@@ -9,6 +9,7 @@
       url = "github:numtide/treefmt-nix";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+
   };
 
   outputs =
@@ -123,6 +124,9 @@
           devShells.default = craneLib.devShell {
             # Inherit inputs from checks.
             checks = self.checks.${system};
+
+            # Additional dev-shell environment variables can be set directly
+            # MY_CUSTOM_DEVELOPMENT_VAR = "something else";
 
             # Extra inputs can be added here; cargo and rustc are provided by default.
             packages = [
