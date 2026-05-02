7d3b92c1eb11a5c53b778c47a12694fe49d8f116
mjadach-iv <michal.jadach@hoprnet.org>
2026-05-02 10:15:36 +0200

    Feat: Single address fund

--- src/components/AddFundsModal.tsx
+++ src/components/AddFundsModal.tsx
@@ -13,20 +13,17 @@ export default function AddFundsModal(props: {
       <div class="flex flex-col gap-8">
         <div class="text-base font-semibold">Add funds</div>
         <div class="flex flex-col gap-4 my-2">
-          <div class="text-xl font-bold">Transfer xDAI</div>
           <FundingAddress
             address={props.nodeAddress}
-            // full
-            title="Transfer xDAI"
+            full
+            qrVisible
           />
-        </div>
-        <div class="flex flex-col gap-4 my-2">
-          <div class="text-xl font-bold">Transfer wxHOPR</div>
-          <FundingAddress
-            address={props.safeAddress}
-            // full
-            title="Transfer wxHOPR"
-          />
+          <div class="text-sm">
+            You can transfer or
+            <span class="font-bold">xDAI</span> or
+            <span class="font-bold">wxHOPR</span> on
+            <span class="font-bold">Gnosis Chain</span>.
+          </div>
         </div>
         <div class="flex flex-row justify-end gap-2">
           <Button size="md" onClick={props.onClose}>
